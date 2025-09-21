8360698f9fc0e425be6473ec8b26543b8c63aaa5
Iasbabu <88984950+Iasbabu@users.noreply.github.com>
2025-09-21 19:49:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,1 @@
-api_id = 20767676 #change_with_your_Api_id.
-api_hash = "7e1fd1a8ec48ad69175548350eae87b2" #change_with_your_Api_hash.
-bot_token = "7780206214:AAGIdCbGo56-_3acSu_GgXeC06SttAQ_1e0" #change_with_your_bot_token.
-auth_users = [5497581790] #change_with_your_user_id.
-sudo_user = 5497581790 #change_with_your_user_id.
-log_channel = -1002850119999 #change_with_your_channel_id.
-txt_channel = -1002850119999 #change_with_your_channel_id.
 
