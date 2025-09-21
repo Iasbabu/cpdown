b32394d9c7b58c3d04afed3cd06a5baeadf61fa2
Iasbabu <88984950+Iasbabu@users.noreply.github.com>
2025-09-21 18:58:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id = 20767676 #change_with_your_Api_id.
+api_hash = "7e1fd1a8ec48ad69175548350eae87b2" #change_with_your_Api_hash.
+bot_token = "7780206214:AAGIdCbGo56-_3acSu_GgXeC06SttAQ_1e0" #change_with_your_bot_token.
+auth_users = [5497581790] #change_with_your_user_id.
+sudo_user = 5497581790 #change_with_your_user_id.
+log_channel = -1002850119999 #change_with_your_channel_id.
+txt_channel = -1002850119999 #change_with_your_channel_id.
+
